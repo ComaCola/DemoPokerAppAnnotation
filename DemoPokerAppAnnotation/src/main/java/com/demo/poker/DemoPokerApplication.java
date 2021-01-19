9d970606ce9fdca6d9f9c2e09705567250bd7dcd
deikan <dk@dk.lt>
2021-01-19 21:38:25 +0200

    first commit

---
+++ DemoPokerAppAnnotation/src/main/java/com/demo/poker/DemoPokerApplication.java
@@ -0,0 +1,17 @@
+package com.demo.poker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Deividas
+ */
+@SpringBootApplication
+public class DemoPokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPokerApplication.class, args);
+    }
+
+}
